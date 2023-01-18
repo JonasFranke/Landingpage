7ec574e29a4d765870acc92416c633f31c78d99b
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2023-01-18 09:14:32 +0000

    add logging

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express";
+import {request, Request, Response} from "express";
 const express = require('express');
 const app = express();
 import fs from 'fs';
@@ -9,17 +9,24 @@ app.get('/', async (req: Request, res: Response) => {
     console.log('GET from' + req.ip);
 
     try {
+        let statusCode: number;
         if (req.header('User-Agent')?.includes('Mobile')) {
             console.log('Mobile');
-            res.status(500).send("Mobile site still in development");
+            statusCode = 503;
+            res.status(statusCode).send("Mobile site still in development");
+            await logDataToFile(statusCode + ": User-Agent: " + req.header('User-Agent'));
         } else {
             console.log('Desktop');
             const html = await fs.promises.readFile('./index.html', 'utf8');
-            res.send(html);
+            statusCode = 200;
+            res.status(statusCode).send(html);
+            await logDataToFile(statusCode + ": User-Agent: " + req.header('User-Agent'));
         }
+
     } catch (e) {
         console.log(e);
         res.status(500).send("Internal server error");
+        await logDataToFile(500 + ": " + e);
     }
 
 });
@@ -27,3 +34,13 @@ app.get('/', async (req: Request, res: Response) => {
 console.log("Running... http://localhost:" + process.env.PORT || 3000);
 
 app.listen(process.env.PORT || 3000);
+
+async function logDataToFile(data: string) {
+    try {
+      await fs.promises.appendFile('log.txt', data);
+      console.log("Request logged to file.");
+    } catch (err) {
+      console.error(err);
+    }
+}
+
