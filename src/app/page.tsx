47f68434dbbe7dc7f7583f9a603fe9fb42e1c518
Jonas Franke <jonas.franke1@gmx.net>
2023-05-27 23:40:10 +0200

    fix headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import InstagramLogoComponent from "@/app/assets/InstagramLogoComponent";
 export default function Home() {
   return (
     <main className="">
-        <p className="text-green-700 text-center font-extrabold mt-32">Jonas Franke</p>
+        <p className="text-green-700 text-center font-extrabold text-4xl mt-32">Jonas Franke</p>
         <div className="flex justify-center items-center relative mt-32">
             <GithubLogoComponent/>
             <InstagramLogoComponent/>
