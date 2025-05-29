e045cd383581a86ca1cc72d874376113ae74c347
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2025-05-29 13:05:18 +0200

    Enable react compiler (#306)

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Bun dev env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bun
+          ];
+
+          shellHook = ''
+            echo "Bun $(bun --version) is ready!" &&
+            export BIOME_BINARY=$(which biome)
+          '';
+        };
+
+    };
+}
