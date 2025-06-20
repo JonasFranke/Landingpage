ef542bde834c826bfc4171e0ea6a69e7600a16aa
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2025-06-20 12:51:39 +0200

    dev/newLogoLib (#323)
    * Update flake
    
    * Biome v2 adjustments
    
    * Move icons to react simple icons

--- flake.nix
+++ flake.nix
@@ -3,22 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    biomepkg.url = "github:isabelroses/nixpkgs/biome-2";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, biomepkg, ... }@inputs: 
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      biome = biomepkg.legacyPackages.${system};
     in
     {
 
       devShells.x86_64-linux.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            bun
+          nativeBuildInputs = [
+            pkgs.bun
+            pkgs.nodejs_24
+            biome.biome
+            pkgs.ffmpeg
           ];
 
           shellHook = ''
-            echo "Bun $(bun --version) is ready!" &&
+            echo "Bun $(bun --version) and Biome $(biome --version) are ready!" &&
             export BIOME_BINARY=$(which biome)
           '';
         };
